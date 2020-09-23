ccd56c5f4d1acc5f8c11e99728c6c6328a46e79a
Jakub Duchon <jakub.duchon9@gmail.com>
2020-09-23 22:27:10 +0200

    feat: add test program

---
+++ main.py
@@ -0,0 +1,12 @@
+from scanner import SourceFile, Scanner
+from tokens import Token, TokenType
+
+fileDir = './testfiles/testprogram.txt'
+
+if __name__ == "__main__":
+    scanner = Scanner(fileDir)
+    token = scanner.scan()
+
+    while token.tokenType != TokenType.EOT:
+        print(f"{token.tokenType}: {token.spelling}")
+        token = scanner.scan()
