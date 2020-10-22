6b98e818ad798c0cbe23353623fb54109eb1b868
Jakub Duchon <jakub.duchon9@gmail.com>
2020-10-22 10:26:15 +0200

    chore: output JSON representation of program

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from scanner import SourceFile, Scanner
 from parser import Parser
+import json
 
 fileDir = './example_files/testprogram.txt'
+
 
 if __name__ == "__main__":
     scanner = Scanner(fileDir)
     parser = Parser(scanner)
 
-    parser.parse_program()
+    program = parser.parse_program()
+
+    print(json.dumps(program, default=lambda x: x.__dict__, indent=2))
