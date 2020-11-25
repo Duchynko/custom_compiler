773387fe1090b9607eda773e5c35bb261d7c8129
Jakub Duchon <jakub.duchon9@gmail.com>
2020-11-25 23:29:26 +0100

    feat(compiler): add encoding to compiler

--- compiler.py
+++ compiler.py
@@ -1,17 +1,20 @@
 from checker import Checker
 from scanner import Scanner
 from parser import Parser
-import json
+from encoder import Encoder
 
-fileDir = './example_files/testprogram.txt'
+fileDir = './example_files/prog1.txt'
 
 
 if __name__ == "__main__":
     scanner = Scanner(fileDir)
     parser = Parser(scanner)
     checker = Checker()
+    encoder = Encoder()
 
     program = parser.parse_program()
     checker.check(program)
+    encoder.encode(program)
+    encoder.save_target_program('examples_files/prog1.tam')
 
     # print(json.dumps(program, default=lambda x: x.__dict__, indent=2))
